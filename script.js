9d43306a4c9cbbffe596945d32540a1dfe7bf9f5
nahuishon <shonn620@newschool.edu>
2021-04-27 15:21:21 +0900

    s

--- script.js
+++ script.js
@@ -12,5 +12,42 @@ function(event){
  inputElement.value;
 
      inputElement.value = "";
+
+     // Loop through 4 times
+    for (let i = 0; i < 4; i++){
+      
+        // Print the number we're at in the loop
+        console.log(i);
+        
+        // Get the i-th letter in the name
+        let letter = name[i];
+        console.log(letter);
+        
+        // Create a new HTML image element for the card
+        let selectedCard = document.createElement("img");
+        
+        // If letter is any of these, use the devil card
+        if (["a", "b", "c", "d", "e", "f"].includes(letter)){
+          selectedCard.src = gerbera.png;
+        }
+        
+        // If letter is any of these, use the magician card
+        if (["g", "h", "i", "j", "k", "l"].includes(letter) ){
+          selectedCard.src = rose.png;
+        }
+        
+        // If letter is any of these, use the star card
+        if (["m", "n", "o", "p", "q", "r"].includes(letter)){
+          selectedCard.src = starURL;
+        }
+        
+        // If letter is any of these, use the moon card
+        if (["s", "t", "u", "v", "w", "x", "y", "z"].includes(letter)){
+          selectedCard.src = moonURL;
+        }
+        
+        result.appendChild(selectedCard);
+        
+      }
   }
 });
