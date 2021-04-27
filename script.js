331d6799ecd04debcfc1783db8da4571c7696196
nahuishon <shonn620@newschool.edu>
2021-04-27 15:34:25 +0900

    d

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let roseURL = "http://127.0.0.1:5500/rose/rose.html";
+let gerberaURL = "http://127.0.0.1:5500/gerbera/gerbera.html";
+let sunURL = "http://127.0.0.1:5500/gerbera/gerbera.html";
+let tulipURL = "http://127.0.0.1:5500/gerbera/gerbera.html";
+
 
 let inputElement = document.querySelector("#input");
 
@@ -6,12 +11,15 @@ let result = document.querySelector("#result");
 inputElement.addEventListener("keydown", 
 function(event){
   if (event.key == "Enter"){
-      //let's show the text on the page!
-      console.log(inputElement.value);
-      result.innerText = "You typed: " +
- inputElement.value;
 
-     inputElement.value = "";
+    let name = inputElement.value;
+    console.log(name);
+
+    //We can also clear what we wrote in the text box
+    inputElement.value = "";
+    
+    //Clear the reading result from last time
+    result.innerHTML = "";
 
      // Loop through 4 times
     for (let i = 0; i < 4; i++){
@@ -28,22 +36,22 @@ function(event){
         
         // If letter is any of these, use the devil card
         if (["a", "b", "c", "d", "e", "f"].includes(letter)){
-          selectedCard.src = gerbera.png;
+          selectedCard.src = roseURL;
         }
         
         // If letter is any of these, use the magician card
         if (["g", "h", "i", "j", "k", "l"].includes(letter) ){
-          selectedCard.src = rose.png;
+          selectedCard.src = gerberaURL;
         }
         
         // If letter is any of these, use the star card
         if (["m", "n", "o", "p", "q", "r"].includes(letter)){
-          selectedCard.src = starURL;
+          selectedCard.src = sunURL;
         }
         
         // If letter is any of these, use the moon card
         if (["s", "t", "u", "v", "w", "x", "y", "z"].includes(letter)){
-          selectedCard.src = moonURL;
+          selectedCard.src = tulipURL;
         }
         
         result.appendChild(selectedCard);
